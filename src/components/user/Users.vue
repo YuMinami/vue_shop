849626a7c92682f7c02622bb1e21b79c85830e5c
YuMinami <e96357@gmail.com>
2020-12-21 17:16:22 +0800

    p51

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -10,23 +10,51 @@
       <!--      搜索与添加-->
       <el-row :gutter="20">
         <el-col :span="8">
-          <el-input placeholder="请输入内容">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="getUserList">
+            <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
           <el-button type="primary">添加用户</el-button>
         </el-col>
       </el-row>
-<!--      用户列表区-->
+      <!--      用户列表区-->
       <el-table :data="userList" border stripe>
+        <el-table-column label="#" type="index"></el-table-column>
         <el-table-column label="姓名" prop="username"></el-table-column>
         <el-table-column label="邮箱" prop="email"></el-table-column>
         <el-table-column label="电话" prop="mobile"></el-table-column>
         <el-table-column label="角色" prop="role_name"></el-table-column>
-        <el-table-column label="状态" prop="mg_state"></el-table-column>
-        <el-table-column label="操作"></el-table-column>
+        <el-table-column label="状态">
+          <template v-slot:default="scope">
+            <el-switch
+              v-model="scope.row.mg_state" @change="userStateChanged(scope.row)">
+            </el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="180px">
+          <template v-slot="">
+            <!--            修改按钮-->
+            <el-button type="primary" size="mini" icon="el-icon-edit"></el-button>
+            <!--            删除按钮-->
+            <el-button type="danger" size="mini" icon="el-icon-delete"></el-button>
+            <!--            分配角色按钮-->
+            <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
+              <el-button type="warning" size="mini" icon="el-icon-setting"></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
       </el-table>
+      <!--分页区域-->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[1, 2, 5, 10]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
     </el-card>
   </div>
 </template>
@@ -38,7 +66,9 @@ export default {
     return {
       queryInfo: {
         query: '',
+        // 当前的页数
         pagenum: 1,
+        // 当前每页显示的条数
         pagesize: 2
       },
       userList: [],
@@ -49,12 +79,36 @@ export default {
     this.getUserList()
   },
   methods: {
-    async getUserList() {
+    async getUserList () {
       const { data: res } = await this.axios.get('users', { params: this.queryInfo })
       console.log(res)
-      if (res.meta.status !== 200) { return this.$message.error('获取用户列表失败') }
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取用户列表失败')
+      }
       this.userList = res.data.users
       this.total = res.data.total
+    },
+    // 监听pagesize改变的事件
+    handleSizeChange (newSize) {
+      // console.log(newSize)
+      this.queryInfo.pagesize = newSize
+      this.getUserList()
+    },
+    // 监听页码值改变的事件
+    handleCurrentChange (newPage) {
+      // console.log(newPage)
+      this.queryInfo.pagenum = newPage
+      this.getUserList()
+    },
+    // 监听switch开关状态改变
+    async userStateChanged (userinfo) {
+      // console.log(userinfo)
+      const { data: res } = await this.axios.put(`users/${userinfo.id}/state/${userinfo.mg_state}`)
+      if (res.meta.status !== 200) {
+        userinfo.mg_state = !userinfo.mg_state
+        return this.$message.error('更新用户状态失败')
+      }
+      this.$message.success('更新用户状态成功!')
     }
   }
 }
