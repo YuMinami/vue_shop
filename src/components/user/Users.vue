ad5bc47926030881f0c0c882d5a7e6b30779bbbf
AirCold <e96357@gmail.com>
2020-12-21 22:40:41 +0800

    p57

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -15,7 +15,7 @@
           </el-input>
         </el-col>
         <el-col :span="4">
-          <el-button type="primary">添加用户</el-button>
+          <el-button type="primary" @click="addDialogVisible = true">添加用户</el-button>
         </el-col>
       </el-row>
       <!--      用户列表区-->
@@ -56,13 +56,54 @@
         :total="total">
       </el-pagination>
     </el-card>
+    <el-dialog
+      title="添加用户"
+      :visible.sync="addDialogVisible"
+      width="50%" @close="addDialogClosed">
+      <!--      内容主体区-->
+      <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机号" prop="mobile">
+          <el-input v-model="addForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <!--      底部区-->
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="addDialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="addUser">确 定</el-button>
+  </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Users',
-  data () {
+  data() {
+    //  验证邮箱规则
+    var checkEmail = (rule, value, callback) => {
+      const regEmail = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+      if (regEmail.test(value)) {
+        return callback()
+      }
+      callback(new Error('请输入正确的邮箱'))
+    }
+    // 验证手机号规则
+    var checkMobile = (rule, value, callback) => {
+      const regMobile = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/
+      if (regMobile.test(value)) {
+        return callback()
+      }
+      callback(new Error('请输入正确的手机号'))
+    }
     return {
       queryInfo: {
         query: '',
@@ -72,16 +113,44 @@ export default {
         pagesize: 2
       },
       userList: [],
-      total: 0
+      total: 0,
+      // 控制添加用户对话框的显示与隐藏
+      addDialogVisible: false,
+      // 添加用户的表单数据
+      addForm: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      // 添加表单的验证规则
+      addFormRules: {
+        username: [{ required: true, message: '请输入用户名', trigger: 'blur' }, {
+          min: 3,
+          max: 10,
+          message: '用户名的长度在3-10个字符之间',
+          trigger: 'blur'
+        }],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }, {
+          min: 6,
+          max: 15,
+          message: '密码的长度在6-15个字符之间',
+          trigger: 'blur'
+        }],
+        email: [{ required: true, message: '请输入邮箱', trigger: 'blur' },
+          { validator: checkEmail, trigger: 'blur' }],
+        mobile: [{ required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }]
+      }
     }
   },
-  created () {
+  created() {
     this.getUserList()
   },
   methods: {
-    async getUserList () {
+    async getUserList() {
       const { data: res } = await this.axios.get('users', { params: this.queryInfo })
-      console.log(res)
+      // console.log(res)
       if (res.meta.status !== 200) {
         return this.$message.error('获取用户列表失败')
       }
@@ -89,19 +158,19 @@ export default {
       this.total = res.data.total
     },
     // 监听pagesize改变的事件
-    handleSizeChange (newSize) {
+    handleSizeChange(newSize) {
       // console.log(newSize)
       this.queryInfo.pagesize = newSize
       this.getUserList()
     },
     // 监听页码值改变的事件
-    handleCurrentChange (newPage) {
+    handleCurrentChange(newPage) {
       // console.log(newPage)
       this.queryInfo.pagenum = newPage
       this.getUserList()
     },
     // 监听switch开关状态改变
-    async userStateChanged (userinfo) {
+    async userStateChanged(userinfo) {
       // console.log(userinfo)
       const { data: res } = await this.axios.put(`users/${userinfo.id}/state/${userinfo.mg_state}`)
       if (res.meta.status !== 200) {
@@ -109,6 +178,26 @@ export default {
         return this.$message.error('更新用户状态失败')
       }
       this.$message.success('更新用户状态成功!')
+    },
+    // 监听添加用户对话框的关闭事件
+    addDialogClosed() {
+      this.$refs.addFormRef.resetFields()
+    },
+    // 点击按钮，添加用户
+    addUser() {
+      this.$refs.addFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.axios.post('users', this.addForm)
+        if (res.meta.status !== 201) {
+          this.$message.error('添加用户失败1')
+          return
+        }
+        this.$message.success('添加用户成功！')
+        // 隐藏添加用户的对话框
+        this.addDialogVisible = false
+        // 重新获取用户列表数据
+        this.getUserList()
+      })
     }
   }
 }
