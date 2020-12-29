d7a1eb0850f25d85ef1c6e7edc6a3c392e0b3fae
AirCold <e96357@gmail.com>
2020-12-29 23:10:07 +0800

    p149导航跳转到商品添加界面

---
+++ src/components/goods/Add.vue
@@ -0,0 +1,19 @@
+<template>
+<div>添加商品</div>
+</template>
+
+<script>
+export default {
+  name: "Add",
+  data() {
+    return {}
+  },
+  created() {
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
