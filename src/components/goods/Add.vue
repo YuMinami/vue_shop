1b692e6a59a5c86a79315279f70f47e26e2f18cc
AirCold <e96357@gmail.com>
2020-12-30 22:01:11 +0800

    p153

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -1,12 +1,52 @@
 <template>
-<div>添加商品</div>
+  <div>
+    <!--    面包屑导航区域-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">
+        首页
+      </el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>添加商品</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!--    卡片区域-->
+    <el-card>
+      <!--      提示区-->
+      <el-alert
+        title="添加商品信息"
+        type="info"
+        center
+        show-icon
+        :closable="false"
+      >
+      </el-alert>
+      <!--      步骤条区-->
+      <el-steps :active="activeIndex" finish-status="success" align-center>
+        <el-step title="基本信息"></el-step>
+        <el-step title="商品参数"></el-step>
+        <el-step title="商品属性"></el-step>
+        <el-step title="商品图片"></el-step>
+        <el-step title="商品内容"></el-step>
+        <el-step title="完成"></el-step>
+      </el-steps>
+      <!--      Tabs标签栏-->
+      <el-tabs :tab-position="'left'" style="height: 200px;">
+        <el-tab-pane label="基本信息">基本信息</el-tab-pane>
+        <el-tab-pane label="商品参数">商品参数</el-tab-pane>
+        <el-tab-pane label="商品属性">商品属性</el-tab-pane>
+        <el-tab-pane label="商品图片">商品图片</el-tab-pane>
+        <el-tab-pane label="商品内容">商品内容</el-tab-pane>
+      </el-tabs>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Add",
+  name: 'Add',
   data() {
-    return {}
+    return {
+      activeIndex: 0
+    }
   },
   created() {
   },
