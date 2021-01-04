492d1f54d9eb2d1b4c5446c9ba2f795a2cc69c98
YuMinami <e96357@gmail.com>
2021-01-04 17:50:50 +0800

    好像删掉了一个import prams 组件，以及p174添加商品失败

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './plugins/element.js'
 import TreeTable from 'vue-table-with-tree-grid'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本对应的样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 // 导入全局样式表
 import './assets/css/global.css'
 import axios from 'axios'
@@ -13,6 +19,7 @@ import VueAxios from 'vue-axios'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.component('tree-table', TreeTable)
+Vue.use(VueQuillEditor)
 Vue.use(VueAxios, axios)
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
